890631d9a43e723d689e26fd1a3363643c1416cd
micsmi2959 <mic.smiechowicz@gmail.com>
2018-05-16 10:51:11 +0200

    validation

--- app/models/spree/favorite.rb
+++ app/models/spree/favorite.rb
@@ -7,6 +7,9 @@ module Spree
 
     validates :product_id, uniqueness: { scope: :user_id, message: Spree.t(:duplicate_favorite), allow_blank: true }
 
+    validates :user_id, uniqueness: { allow_blank: true, message: 'Already added as favorite.', scope: [:product_id] }
+    validates :guest_token, uniqueness: { allow_blank: true, message: 'Already added as favorite.', scope: [:product_id] }
+
     scope :with_product_id, ->(id) { joins(:product).readonly(false).merge(Spree::Product.where(id: id)) }
     scope :by_guest_token, -> (token) { where(guest_token: token) }
 
