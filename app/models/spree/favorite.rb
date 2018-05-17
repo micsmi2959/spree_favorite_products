60c8c212030e2ca6b9d7076b3aaf56fa33ecd955
micsmi2959 <mic.smiechowicz@gmail.com>
2018-05-17 15:28:24 +0200

    removed user required attr for favorite

--- app/models/spree/favorite.rb
+++ app/models/spree/favorite.rb
@@ -1,9 +1,9 @@
 module Spree
   class Favorite < ActiveRecord::Base
-    with_options required: true do
+    #with_options required: true do
       belongs_to :product, counter_cache: :favorite_users_count
       belongs_to :user
-    end
+    #end
 
     validates :product_id, uniqueness: { scope: :user_id, message: Spree.t(:duplicate_favorite), allow_blank: true }
 
